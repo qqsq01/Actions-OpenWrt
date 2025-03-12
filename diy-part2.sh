ab231e7f4c691fe0e29583abbae07fb741e71626
qqsq01 <61352826+qqsq01@users.noreply.github.com>
2025-03-12 13:33:56 +0800

    diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,14 +21,3 @@
 
 
 # Custom
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
-cp -f feeds/smpackage/.github/diy/default-settings package/emortal/default-settings/files/99-default-settings
-rm -rf feeds/packages/net/{alist,adguardhome,brook,gost,mosdns,redsocks*,smartdns,trojan*,v2ray*,xray*}
-rm -rf feeds/packages/luci/{luci-app-homeproxy,luci-app-openclash,luci-app-passwall}
-rm -rf feeds/smpackage/luci-theme-design && git clone -b js --single-branch https://github.com/kenzok78/luci-theme-design feeds/smpackage/luci-theme-design
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-patch -p1 < "feeds/smpackage/.github/diy/patches/0004-luci-mod-status-firewall-disable-legacy-firewall-rul.patch"
-cp -f feeds/smpackage/.github/diy/banner package/base-files/files/etc/banner
-sed -i "s/%D %V, %C/immortalwrt $(date +'%m.%d') by qqsq/g" package/base-files/files/etc/banner
-./scripts/feeds update -a && ./scripts/feeds install -a
